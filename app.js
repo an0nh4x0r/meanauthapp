5b86a199dd08bde8e021c73603dbcea445435c9f
Swapnil Singh <swappp7singh@gmail.com>
2017-02-16 23:46:37 +0530

    created user model and added database connection using mongoose

--- app.js
+++ app.js
@@ -4,21 +4,44 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// Connect to the database
+mongoose.connect(config.database);
+
+// On connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to the database ' + config.database);
+});
+
+// On error
+mongoose.connection.on('error', (err) => {
+    console.log('Database error: ' + err);
+});
 
 const app = express();
+
+const users = require('./routes/users');
 
 const port = 3000;
 
 // cors middleware
 app.use(cors());
 
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // body-parse middleware
 app.use(bodyParser.json());
 
+app.use('/users', users);
+
+// Index route
 app.get('/', (req, res) => {
     res.send('Invalid Endpoint.');
 });
 
+// Start server
 app.listen(port, () => {
     console.log("Server started on port " + port);
 });
